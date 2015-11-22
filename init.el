72926f608c8d65ec1ad3f3d5c35ae9094d129287
Akira Baruah <akira.baruah@gmail.com>
2015-11-22 21:43:25 +0000

    .emacs.d/init.el: remove orgmode package archive

--- init.el
+++ init.el
@@ -6,7 +6,6 @@
   (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
   (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
   (package-initialize)
   )
 
