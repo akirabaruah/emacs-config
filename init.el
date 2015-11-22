c3deb1f1cf6d3dd0aac2f82bb6a70148dc1d3fc2
Akira Baruah <akira.baruah@gmail.com>
2015-11-22 21:39:31 +0000

    Add some basic emacs stuff

---
+++ init.el
@@ -0,0 +1,17 @@
+;; Akira Baruah's emacs config
+
+;; Emacs Lisp Package Archive (ELPA)
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
+  (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+  (package-initialize)
+  )
+
+;; Save backups to /tmp
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
