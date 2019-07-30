a210812f8291a836a59d98fad0f24c15866fd4dc
Akira Baruah <akira.baruah@gmail.com>
2019-07-30 00:34:33 -0700

    Update emacs config

--- init.el
+++ init.el
@@ -1,25 +1,33 @@
 ;; Akira Baruah's emacs config
 
-;; Emacs Lisp Package Archive (ELPA)
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
-  (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-  (package-initialize)
-  )
+;; TODO(https://debbugs.gnu.org/34341): Remove this workaround once fix lands.
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
+;; Package archives
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; Install use-package if needed
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Enable use-package
+(eval-when-compile
+  (require 'use-package))
+
+;; Enable evil-mode
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode))
 
 ;; Save backups to /tmp
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
-
-;; Indentation
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4) ; or any other preferred value
-(defvaralias 'c-basic-offset 'tab-width)
-(defvaralias 'cperl-indent-level 'tab-width)
 
 ;; Delete trailing whitespace on save
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
