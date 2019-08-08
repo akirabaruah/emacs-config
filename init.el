e353f32c835bead4463c726c74cbe8c6ecaf0f11
Akira Baruah <akira.baruah@gmail.com>
2019-08-08 15:19:58 -0700

    [emacs] Add Packages section to Org config
    Moves Evil's use-package definition from init.el to README.org and
    adds General and Hydra packages under the new Packages section.

--- init.el
+++ init.el
@@ -33,14 +33,6 @@
 (eval-when-compile
   (require 'use-package))
 
-;; Enable evil-mode
-(use-package evil
-  :ensure t
-  :init
-  (setq evil-want-C-i-jump nil)  ;; C-i is also <tab>; don't use it for evil
-  :config
-  (evil-mode))
-
 ;; Enable Org
 (use-package org :ensure org-plus-contrib)
 
